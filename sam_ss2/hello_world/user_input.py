f1b384df570a3c30e10470d4437d29bcac158ddf
sam <samsoncoded@gmail.com>
2023-06-07 16:22:41 +0100

    moving my class activities to github

---
+++ sam_ss2/hello_world/user_input.py
@@ -0,0 +1,7 @@
+# age calculator
+# user input stored in name variable
+name = input("what is your name?")
+birth_year = input("what year where you born?")
+current_year = 2023
+age = int(current_year) - int(birth_year)
+print(f"hi,{name} ,you are {age} years old")
---
+++ sam_ss2/hello_world/variables.py
@@ -0,0 +1,16 @@
+# addtion of two numbers
+# printing integers
+x = 5
+X = 6
+y = 8
+Y = 10
+equation = (2*x+3*y)*(2*X+3*Y)
+print(equation)
+
+# printing strings
+small_alpha = "a"
+big_alpha = "A"
+print(small_alpha)
+print(big_alpha)
+
+
